ad557061f041a2e18951f9f1904097cd4215599f
Gabriel Santos <gabriel.santos@tmaior.com.br>
2024-01-18 01:38:21 -0300

    Finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
-class ApplicationController < ActionController::
-    def hello
-        render html: "hello, world"
-    end
+class ApplicationController < ActionController::Base
+  def hello
+    render html: "hello, world"
+  end
 end
